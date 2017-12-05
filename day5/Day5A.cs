53d456a8d6c90b1c3a6ae29eb28a07d11823efc6
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-05 10:19:20 +0100

    Add files via upload

---
+++ day5/Day5A.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp19
+{
+    class Day5A
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input5").Select(int.Parse).ToList();
+            int pos = 0, steps = 0;
+
+            while (pos < input.Count && pos >= 0)
+            {
+                pos += input[pos]++;
+                steps++;
+            }
+            Console.WriteLine(steps);
+        }
+    }
+}
