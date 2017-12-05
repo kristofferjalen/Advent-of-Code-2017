d4d2c91e0446bc48179f4a872f7aa6a022f53d12
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-05 15:46:39 +0100

    Add files via upload

---
+++ day4/Day4A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleApp18
+{
+    class Day4A
+    {
+        static void Main()
+        {
+            var passphrases = File.ReadAllLines("input").Select(x => x.Split(" ").ToList());
+
+            var valids = passphrases.Count(p => p.Distinct().Count() == p.Count);
+            
+            Console.WriteLine(valids);
+        }
+    }
+}
