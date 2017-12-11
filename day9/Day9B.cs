5560fe07bbd31e74f493d52221fc2070dc290543
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-11 15:39:48 +0100

    Add files via upload

---
+++ day9/Day9B.cs
@@ -0,0 +1,29 @@
+﻿class Day9B
+{
+    static void Main()
+    {
+        var lines = System.IO.File.ReadAllLines("input");
+
+        var removed = 0;
+
+        foreach (var line in lines)
+        {
+            var garbage = false;
+
+            for (var i = 0; i < line.Length; i++)
+            {
+                var c = line[i];
+                if (c == '!')
+                    i++;
+                else if (garbage && c == '>')
+                    garbage = false;
+                else if (garbage)
+                    removed++;
+                else if (c == '<')
+                    garbage = true;
+            }
+        }
+
+        System.Console.WriteLine(removed); // 5101
+    }
+}
