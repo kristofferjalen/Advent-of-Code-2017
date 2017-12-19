10dd8ce364a4c5f2683d6e8b2b4ca723a776d82d
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-19 22:01:13 +0100

    Add files via upload

---
+++ day11/Day11A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Day11A
+    {
+        static void Main()
+        {
+            var input = System.IO.File.ReadAllText("input");
+
+            int x = 0, y = 0;
+            
+            var f = new Dictionary<string, Func<int, int, (int, int)>>
+            {
+                {"ne", (_, __) => (_ + 1, __ - 1)},
+                {"n", (_, __) => (_, __ - 2)},
+                {"nw", (_, __) => (_ - 1, __ - 1)},
+                {"se", (_, __) => (_ + 1, __ + 1)},
+                {"s", (_, __) => (_, __ + 2)},
+                {"sw", (_, __) => (_ - 1, __ + 1)}
+            };
+            
+            foreach (var op in input.Split(","))
+            {
+                (x, y) = f[op](x, y);
+            }
+
+            //var steps = Math.Abs(x) + Math.Abs((y > 0 ? y - Math.Abs(x) : y + Math.Abs(x)) / 2);
+            var steps = (Math.Abs(y) + Math.Abs(x)) / 2;
+            
+            Console.WriteLine(steps); // 685
+        }
+    }
+}
---
+++ day11/Day11B.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Day11B
+    {
+        static void Main()
+        {
+            var input = System.IO.File.ReadAllText("input");
+
+            int x = 0, y = 0, max = 0;
+            
+            var f = new Dictionary<string, Func<int, int, (int, int)>>
+            {
+                {"ne", (_, __) => (_ + 1, __ - 1)},
+                {"n", (_, __) => (_, __ - 2)},
+                {"nw", (_, __) => (_ - 1, __ - 1)},
+                {"se", (_, __) => (_ + 1, __ + 1)},
+                {"s", (_, __) => (_, __ + 2)},
+                {"sw", (_, __) => (_ - 1, __ + 1)}
+            };
+            
+            foreach (var op in input.Split(","))
+            {
+                (x, y) = f[op](x, y);
+                max = Math.Max(max, (Math.Abs(y) + Math.Abs(x)) / 2);
+            }
+            
+            Console.WriteLine(max); // 1457
+        }
+    }
+}
