1f88c939c4c2a55d9b2e18e4a32fccbdb11a325c
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-19 21:59:34 +0100

    Add files via upload

---
+++ day12/Day12A.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Day12A
+    {
+        static void Main()
+        {
+            var lines = System.IO.File.ReadAllLines("input");
+
+            var group = new List<int>{0};
+            group.AddRange(lines[0].Split(" <-> ")[1].Split(", ").Select(int.Parse));
+
+            var @continue = true;
+
+            while (@continue)
+            {
+                @continue = false;
+                
+                foreach (var line in lines.Skip(1))
+                {
+                    var tokens = line.Split(" <-> ");
+                    var programs = tokens[1].Split(", ").Select(int.Parse);
+
+                    foreach (var p in programs)
+                    {
+                        if (group.Any(x => x == p) && group.All(x => x != int.Parse(tokens[0])))
+                        {
+                            group.Add(int.Parse(tokens[0]));
+                            @continue = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var sum = group.Count;
+
+            Console.WriteLine(sum); // 306
+        }
+    }
+}
---
+++ day12/Day12B.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public static class Extensions
+    {
+        public static int GetProgram(this string s) => int.Parse(s.Split(" <-> ")[0]);
+
+        public static IEnumerable<int> GetPipes(this string s) => s.Split(" <-> ")[1].Split(", ").Select(int.Parse);
+    }
+
+    class Day12B
+    {
+        static void Main()
+        {
+            var lines = System.IO.File.ReadAllLines("input");
+
+            var groups = new List<List<int>>();
+
+            while (lines.Length > groups.SelectMany(x => x).Distinct().Count())
+            {
+                var first = lines.First(x => !groups.SelectMany(y => y).Contains(x.GetProgram()));
+                groups.Add(new List<int>(first.GetPipes().Append(first.GetProgram())));
+
+                var @continue = true;
+
+                while (@continue)
+                {
+                    @continue = false;
+
+                    foreach (var line in lines.Skip(1))
+                    {
+                        foreach (var p in line.GetPipes())
+                        {
+                            var g = groups.SingleOrDefault(x => x.Contains(p) && !x.Contains(line.GetProgram()));
+
+                            if (g == null)
+                                continue;
+
+                            g.Add(line.GetProgram());
+                            @continue = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(groups.Count); // 200 
+        }
+    }
+}
