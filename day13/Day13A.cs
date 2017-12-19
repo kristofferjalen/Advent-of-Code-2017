89fa01d8a8cba33570b5fa743ee3c9dd9ad7b0a4
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-19 21:57:09 +0100

    Add files via upload

---
+++ day13/Day13A.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp12
+{
+    public class Layer
+    {
+        public int Depth { get; set; }
+
+        public int Range { get; set; }
+
+        public int Position { get; set; }
+
+        public int Direction { get; set; } = -1;
+    }
+
+    class Day13A
+    {
+
+        static void Main()
+        {
+            var lines = System.IO.File.ReadAllLines("input");
+
+            var layers = lines.Select(x => new Layer
+            {
+                    Depth = int.Parse(x.Split(": ")[0]),
+                    Range = int.Parse(x.Split(": ")[1])
+                }
+            ).ToList();
+
+            var i = 0;
+            var severity = 0;
+
+            while (i <= layers.Max(x => x.Depth))
+            {
+                severity += layers.Where(x => x.Depth == i && x.Position == 0).Sum(x => x.Depth * x.Range);
+
+                i++;
+
+                foreach (var layer in layers)
+                {
+                    if (layer.Position == layer.Range - 1 || layer.Position == 0)
+                        layer.Direction *= -1;
+
+                    layer.Position += layer.Direction;
+                }
+            }
+
+            Console.WriteLine(severity); // 1900
+        }
+    }
+}
---
+++ day13/Day13B.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Layer
+    {
+        public int Depth { get; set; }
+
+        public int Range { get; set; }
+
+        public int Position { get; set; }
+
+        public int LastPosition { get; set; }
+
+        public int LastDirection { get; set; } = -1;
+
+        public int Direction { get; set; } = -1;
+    }
+
+    class Day13B
+    {
+        static void Main()
+        {
+            var lines = System.IO.File.ReadAllLines("input");
+            
+            var layers = lines.Select(x => new Layer
+                {
+                    Depth = int.Parse(x.Split(": ")[0]),
+                    Range = int.Parse(x.Split(": ")[1])
+                }
+            ).ToList();
+
+            var delay = 1;
+            bool caught;
+
+            do
+            {
+                var i = 0;
+                caught = false;
+                
+                foreach (var layer in layers)
+                {
+                    layer.Position = layer.LastPosition;
+                    layer.Direction = layer.LastDirection;
+                    
+                    if (layer.Position == layer.Range - 1 || layer.Position == 0)
+                            layer.Direction *= -1;
+
+                    layer.Position += layer.Direction;
+                    layer.LastPosition = layer.Position;
+                    layer.LastDirection = layer.Direction;
+                }
+
+                while (!caught && i <= layers.Max(x => x.Depth))
+                {
+                    caught = layers.Any(x => x.Depth == i && x.Position == 0);
+
+                    i++;
+
+                    foreach (var layer in layers)
+                    {
+                        if (layer.Position == layer.Range - 1 || layer.Position == 0)
+                            layer.Direction *= -1;
+
+                        layer.Position += layer.Direction;
+                    }
+                }
+
+                if (caught)
+                    delay++;
+                
+            } while (caught);
+            
+            Console.WriteLine(delay); // 3966414
+        }
+    }
+}
