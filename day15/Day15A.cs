076c7d77b811a57854226ac2e0f1472ae0446687
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-22 16:20:27 +0100

    Add files via upload

---
+++ day15/Day15A.cs
@@ -0,0 +1,19 @@
+﻿class Day15A
+{
+    static void Main()
+    {
+        long a = 873, b = 583;
+        int matches = 0, i = 0;
+
+        while (i++ < 40_000_000)
+        {
+            a = a * 16807 % 2147483647;
+            b = b * 48271 % 2147483647;
+
+            if ((a & 0b1111_1111_1111_1111) == (b & 0b1111_1111_1111_1111))
+                matches++;
+        }
+
+        System.Console.WriteLine(matches); // 631
+    }
+}
---
+++ day15/Day15B.cs
@@ -0,0 +1,26 @@
+﻿class Day15B
+{
+    static void Main()
+    {
+        long a = 873, b = 583;
+        int matches = 0, i = 0;
+
+        while (i++ < 5_000_000)
+        {
+            do
+            {
+                a = a * 16807 % 2147483647;
+            } while (a % 4 != 0);
+
+            do
+            {
+                b = b * 48271 % 2147483647;
+            } while (b % 8 != 0);
+
+            if ((a & 0b1111_1111_1111_1111) == (b & 0b1111_1111_1111_1111))
+                matches++;
+        }
+
+        System.Console.WriteLine(matches); // 279
+    }
+}
