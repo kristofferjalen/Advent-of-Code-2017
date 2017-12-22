cef8d5dc02be45ac12bc912cf008bec3ec237f45
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-22 16:22:53 +0100

    Add files via upload

---
+++ day17/Day17B.cs
@@ -0,0 +1,17 @@
+﻿class Day17B
+{
+    static void Main()
+    {
+        int after0 = 0, curr = 0, i = 1;
+
+        while (i <= 50_000_000)
+        {
+            curr = (curr + 370) % i++;
+
+            if (curr++ == 0)
+                after0 = i - 1;
+        }
+
+        System.Console.WriteLine(after0); // 11162912
+    }
+}
