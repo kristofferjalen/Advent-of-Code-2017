80f02eea99b9e5d4fc66688ef8d5832a8d61d557
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2018-01-23 16:26:45 +0100

    Add files via upload

---
+++ day02/Day2A.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+class Day2A
+{
+    static void Main()
+    {
+        var checksum = System.IO.File.ReadAllLines("input")
+            .Select(x => x.Split("\t").Select(int.Parse).ToList())
+            .Sum(x => x.Max() - x.Min());
+
+        System.Console.WriteLine(checksum); // 32121
+    }
+}
---
+++ day02/Day2B.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+class Day2B
+{
+    static void Main()
+    {
+        var sum = System.IO.File.ReadAllLines("input").Select(x => x.Split("\t").Select(int.Parse).ToList())
+            .SelectMany(line => line, (line, i) => line
+                .Where(x => x != i && x % i == 0)
+                .Sum(x => x / i))
+            .Sum();
+
+        System.Console.WriteLine(sum); // 197
+    }
+}
