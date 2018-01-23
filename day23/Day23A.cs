ed2607f5519a40b87ed2b81148238c2680000a59
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2018-01-23 15:39:01 +0100

    Add files via upload

---
+++ day23/Day23A.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Day23A
+    {
+        static void Main()
+        {
+            var instructions = System.IO.File.ReadAllLines("input");
+
+            var registers = new Dictionary<string, long>
+            {
+                {"a", 0},
+                {"b", 0},
+                {"c", 0},
+                {"d", 0},
+                {"e", 0},
+                {"f", 0},
+                {"g", 0},
+                {"h", 0}
+            };
+
+            int count = 0;
+
+            var i = 0;
+
+            while (i >= 0 && i < instructions.Count())
+            {
+                var args = instructions[i].Split(' ');
+                var op = args[0];
+                var arg1 = args[1];
+                var arg2 = args.Length == 3 ? args[2] : "";
+
+                if (op == "set")
+                {
+                    registers[arg1] = long.TryParse(arg2, out var result) ? result : registers[arg2];
+                }
+                else if (op == "sub")
+                {
+                    registers[arg1] -= long.TryParse(arg2, out var result) ? result : registers[arg2];
+                }
+                else if (op == "mul")
+                {
+                    registers[arg1] *= long.TryParse(arg2, out var result) ? result : registers[arg2];
+                    count++;
+                }
+                else if (op == "jnz")
+                {
+                    long result = long.TryParse(arg1, out result) ? result : registers[arg1];
+                    if (result != 0)
+                    {
+                        long jump = long.TryParse(arg2, out jump) ? jump : registers[arg2];
+                        i += (int)jump - 1;
+                    }
+                }
+
+                i++;
+            }
+
+            Console.WriteLine(count); // 
+        }
+    }
+}
---
+++ day23/Day23B.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Day23B
+    {
+        static void Main()
+        {
+            var b = 99 * 100 + 100000;
+            var c = b + 17000;
+            var h = 0;
+
+            while (true)
+            {
+                var f = 1;
+
+                // How many numbers are composite (non-prime)?
+                for (var d = 2; d < b; d++)
+                {
+                    if (b % d == 0)
+                    {
+                        f = 0;
+                        break;
+                    }
+                }
+
+                if (f == 0)
+                    h++;
+
+                if (b == c)
+                    break;
+
+                b += 17;
+            }
+
+            Console.WriteLine(h); // 913
+        }
+    }
+}
