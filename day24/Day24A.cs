1be2eb7609ae31054cbd1331ed135a85319a2429
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2018-01-23 15:40:05 +0100

    Add files via upload

---
+++ day24/Day24A.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Day24A
+    {
+        static void Main()
+        {
+            var components = System.IO.File.ReadAllLines("input")
+                .Select(x => (int.Parse(x.Split("/")[0]), int.Parse(x.Split("/")[1]))).ToList();
+
+            var strength = Rec(new List<(int, int)>(), 0, components)
+                .Max(x => x.Sum(c => c.Item1 + c.Item2));
+
+            System.Console.WriteLine(strength); // 1656
+        }
+
+        private static IEnumerable<List<(int, int)>> Rec(ICollection<(int, int)> list, int port, List<(int, int)> components)
+        {
+            var result = new List<List<(int, int)>>();
+
+            foreach (var c in components.Where(x => !list.Contains(x) && (x.Item1 == port || x.Item2 == port)))
+            {
+                var newList = new List<(int, int)>(list) {c};
+                result.Add(newList);
+                result.AddRange(Rec(newList, c.Item1 == port ? c.Item2 : c.Item1, components));
+            }
+
+            return result;
+        }
+    }
+}
